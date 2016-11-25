8468fa2aad097d792ec8fef1c8baeb2a83301d4f
voksepp <fredrik.voksepp@gmail.com>
2016-11-25 23:04:06 +0100

    v1

--- src/Lab3.java
+++ src/Lab3.java
@@ -1,2 +1,14 @@
+import Lab3Help.*;
+
+import java.io.IOException;
+import java.util.List;
+
 public class Lab3 {
+
+    public static void main (String[] args) throws IOException, MalformedData {
+        Lab3File lab3File = new Lab3File();
+        List<BStop> bStopList = lab3File.readStops(args[0]);
+        List<BLineTable> bLineTableList = lab3File.readLines(args[1]);
+        // BLineStop stopA =
+    }
 }
