c2c2ad609b098146008d98ad157509942dd1fd84
voksepp <fredrik.voksepp@gmail.com>
2016-11-29 12:18:53 +0800

    v1

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,2 +1,21 @@
-public class Vertex {
+import java.util.ArrayList;
+
+public class Vertex<E> implements Comparable<Vertex>{
+    private final ArrayList<Vertex> adjList;
+    private final String name;
+
+    public Vertex(String name, ArrayList<Vertex> adjList) {
+        this.name = name;
+        this.adjList = adjList;
+    }
+    public String getName(){
+        return name;
+    }
+    public ArrayList<Vertex> getAdjList(){
+        return adjList;
+    }
+    @Override
+    public int compareTo(Vertex o) {
+        return 0;
+    }
 }
