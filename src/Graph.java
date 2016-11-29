0ee6c7e763b273fe695c71f059ee9ee5d8fd1869
Erik <erikforsstrom.kode@gmail.com>
2016-11-29 16:21:27 +0100

    1

--- src/Graph.java
+++ src/Graph.java
@@ -3,10 +3,20 @@ import Lab3Help.*;
 import java.util.List;
 
 public class Graph {
-    List<BStop> stops;
-    List<BLineTable> lines;
-    void Graph(List<BStop> stops, List<BLineTable> lines){
+
+    private List<BStop> stops;
+    private List<BLineTable> lines;
+
+    public Graph(List<BStop> stops, List<BLineTable> lines){
         this.stops = stops;
         this.lines = lines;
     }
+
+    public void setStops(){
+        for(BStop bSt : stops){
+
+        }
+    }
+
+
 }
--- src/Node.java
+++ src/Node.java
@@ -1,14 +1,17 @@
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
 public class Node<E> {
     public final String name;
     public final Map<String, Object> node;
+    public List<Node<E>> predNodes;
+    public List<Node<E>> succNodes;
 
     public Node(Map<String, Object> init){
         node = new HashMap<>(init);
-        name = toString(); //FIXME: Overrida toString med egen?? Alt. Ändra denna.
+        name = toString(); //FIXME: Overrida toString med egen?? Alt. Ändra denna. Är den ens nödvändigt? Vad är meningen med livet?
     }
 
     Object get(String var){
@@ -16,5 +19,18 @@ public class Node<E> {
             return node.get(var);
         }
         throw new NoSuchElementException();
+    }
+
+    public void setPredNodes(List<Node<E>> n){
+        for (Node<E> nd : n){
+            predNodes.add(nd);
+        }
+    }
+
+    public void succNode(List<Node<E>> n){
+        for(Node<E> nd : n){
+            succNodes.add(nd);
+        }
+
     }
 }
