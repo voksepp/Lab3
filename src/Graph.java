0c045e197bde89ec7aee440f7ae01c55dc26b1de
Erik <erikforsstrom.kode@gmail.com>
2016-11-29 16:02:05 +0100

    1

--- src/Graph.java
+++ src/Graph.java
@@ -1,3 +1,12 @@
-public class Graph {
+import Lab3Help.*;
 
+import java.util.List;
+
+public class Graph {
+    List<BStop> stops;
+    List<BLineTable> lines;
+    void Graph(List<BStop> stops, List<BLineTable> lines){
+        this.stops = stops;
+        this.lines = lines;
+    }
 }
--- src/Node.java
+++ src/Node.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
-public class Node {
+public class Node<E> {
     public final String name;
     public final Map<String, Object> node;
 
