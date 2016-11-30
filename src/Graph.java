321f32a078775a0f81dec542616d4f1b208bcc95
Erik <erikforsstrom.kode@gmail.com>
2016-11-30 10:57:46 +0100

    2

--- src/Graph.java
+++ src/Graph.java
@@ -7,9 +7,17 @@ public class Graph<T> {
     private List<Vertex<T>> vertices;
     private List<Edge<T>> edges;
 
+    private List<T> busStops;
+    private List<T> lineTable;
+
     public Graph(){
         vertices = new ArrayList<Vertex<T>>;
         edges = new ArrayList<Edge<T>>();
+    }
+
+    public Graph(List<T> busStops, List<T> lineTable){
+        this.busStops = busStops;
+        this.lineTable = lineTable;
     }
 
     public void setStops(){
