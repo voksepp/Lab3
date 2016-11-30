d09e317da3cb3edadf07d9a40ad43418de89529e
voksepp <fredrik.voksepp@gmail.com>
2016-11-30 16:35:50 +0100

    v1

--- src/Lab3.java
+++ src/Lab3.java
@@ -5,9 +5,36 @@ import java.util.List;
 public class Lab3 {
 
     public static void main (String[] args) throws IOException, MalformedData {
+        if (args.length != 4)
+            throw new IllegalArgumentException("Usage: java Lab3 stops.txt lines.txt HållplatsA HållplatsB");
+
         Lab3File fileParser = new Lab3File();
         List<BStop> stops = fileParser.readStops("stops-gbg.txt");
         List<BLineTable> lines = fileParser.readLines("lines-gbg.txt");
+        Graph g = new Graph(stops, lines);
+        DijkstraPath dp = new DijkstraPath(g);
+
         //new GUI(stops, lines, new DijkstraStringPath(stops, lines));
+
+        String from = args[2];
+        String to = args[3];
+        Vertex fromStop = g.getVertex(from);
+        Vertex toStop = g.getVertex(to);
+
+        dp.computePath(fromStop, toStop);
+
+        if(dp.getPathLength() > 0){
+            System.out.println(dp.getPathLength());
+            while (dp.getPath().hasNext())
+                System.out.println(dp.getPath().next());
+        }
+        else if(args[2] == args[3]){
+            System.out.println(0);
+            System.out.println(args[2]);
+        }
+        else if( //TODO: NOPATH ){
+            System.out.println("Det finns ingen väg från " + args[]);
+        }
+
     }
 }
