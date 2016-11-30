3bf1246e0aef61ac1faa66c148791da037c27bd0
voksepp <fredrik.voksepp@gmail.com>
2016-11-30 11:08:05 +0100

    v1

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
 
 public class Vertex<E> implements Comparable<Vertex>{
-    private final ArrayList<Vertex> adjList;
+    private ArrayList<Vertex> incoming;
+    private ArrayList<Vertex> outgoing;
     private final String name;
 
     public Vertex(String name, ArrayList<Vertex> adjList) {
