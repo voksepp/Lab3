e365dcf7c2d4c818cb055c8df349b234c4300344
voksepp <fredrik.voksepp@gmail.com>
2016-11-30 14:52:34 +0100

    v1

--- src/DijkstraPath.java
+++ src/DijkstraPath.java
@@ -3,7 +3,6 @@ import Lab3Help.Path;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
 
 public class DijkstraPath<E> implements Path<E> {
 
