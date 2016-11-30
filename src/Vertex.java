1bdaca95a77052be8856ed74e71646766a1f0a70
voksepp <fredrik.voksepp@gmail.com>
2016-11-30 11:42:49 +0100

    v1

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,28 +1,29 @@
 import java.util.ArrayList;
 
 public class Vertex<T> implements Comparable<Vertex>{
-    private ArrayList<Vertex<T>> incoming;
-    private ArrayList<Vertex<T>> outgoing;
+    private ArrayList<Edge<T>> incoming;
+    private ArrayList<Edge<T>> outgoing;
     private final String name;
 
-    public Vertex(String name, ArrayList<Vertex<T>> incoming, ArrayList<Vertex<T>> outgoing) {
+    public Vertex(String name) {
         this.name = name;
-        this.incoming = incoming;
-        this.outgoing = outgoing;
+        incoming = new ArrayList<Edge<T>>();
+        outgoing = new ArrayList<Edge<T>>();
     }
-    public boolean addEdge(){
-
+    public boolean addEdge(Edge<T> edge){
+        if(edge.getFrom() == this)
+            outgoing.add(edge);
     }
 
     public String getName(){
         return name;
     }
 
-    public ArrayList<Vertex<T>> getIncoming(){
+    public ArrayList<Edge<T>> getIncoming(){
         return incoming;
     }
 
-    public ArrayList<Vertex<T>> getOutgoing(){
+    public ArrayList<Edge<T>> getOutgoing(){
         return outgoing;
     }
 
