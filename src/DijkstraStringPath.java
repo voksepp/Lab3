ef854b686be88a06ef0580b310e31a892d02b335
voksepp <fredrik.voksepp@gmail.com>
2016-11-30 13:14:49 +0100

    v1

--- src/DijkstraStringPath.java
+++ src/DijkstraStringPath.java
@@ -57,6 +57,6 @@ public class DijkstraStringPath implements Path<String> {
      */
     @Override
     public int getPathLength() {
-        //return d.computePath();
+        return d.computePath();
     }
 }
