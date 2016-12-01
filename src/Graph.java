fce9bf7341d8426e4ba2f1d5498e39701edd142d
voksepp <fredrik.voksepp@gmail.com>
2016-12-01 22:09:59 +0100

    v1

--- src/Graph.java
+++ src/Graph.java
@@ -1,32 +1,53 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.security.InvalidParameterException;
+import java.util.*;
 
-public class Graph<V, E> {
-    private Map<V, Vertex<V>> vMap = new HashMap<>();
-    private List<V> vertices;
+public class Graph<V> {
+
+    private Map<V, Vertex<V>> vMap;
 
     public Graph(){
-        vertices = new ArrayList<>();
-    }
-
-    public boolean isEmpty(){
-        return vertices.isEmpty();
+        vMap = new HashMap<>();
     }
 
     public int size(){
-        return vertices.size();
+        return vMap.size();
     }
 
-    public List<V> getVertices(){
-        return vertices;
+    public Vertex<V> findNode(V lab) {
+        Vertex<V> gotten = vMap.get(lab);
+        if(gotten != null) return gotten;
+        else return null;
     }
 
-    public void addVertex(V vertex){
-        if (!vertices.contains(vertex))
-            vertices.add(vertex);
+    public Vertex<V> addVertex(V lab)  {
+        if(findNode(lab) != null) return null;
+        Vertex<V> newGuy = new Vertex<V>(lab);
+        vMap.put(lab, newGuy);
+        return newGuy;
     }
+
+    public List<Vertex<V>> getVertices() {
+        ArrayList<Vertex<V>> vertexList = new ArrayList<>();
+        Collection<Vertex<V>> collection = vMap.values();
+        Iterator<Vertex<V>> it = collection.iterator();
+        while(it.hasNext()){
+            vertexList.add(it.next());
+        }
+        return vertexList;
+    }
+
+    public Edge<V> addEdge(V from, int cost, V to){
+        if (from.equals(to))
+            return null;
+        Vertex<V> fromV = findNode(from);
+        Vertex<V> toV = findNode(to);
+
+        Edge<V> e = new Edge<V>(fromV, toV, cost);
+        fromV.addOutEdge(e);
+
+        return e;
+    }
+
 
     public boolean addEdge(V source, V target, int cost){
 
