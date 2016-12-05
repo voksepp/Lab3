3b0a0bfa88f12dcc92676259c25ddf39b5f98cfb
voksepp <fredrik.voksepp@gmail.com>
2016-12-05 20:00:28 +0100

    v1

--- src/Vertex.java
+++ src/Vertex.java
@@ -5,7 +5,6 @@ public class Vertex<V,E> {
     private final V name;
     public Vertex(V name) {
         this.name = name;
-        outEdges = new ArrayList<>();
     }
     public String toString(){
         return name.toString();
