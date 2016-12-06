bfaba2cf250b5dd68056b99ac25bf5cd80d67c2e
Erik <erikforsstrom.kode@gmail.com>
2016-12-06 15:45:05 +0100

    4

--- src/Lab3.java
+++ src/Lab3.java
@@ -12,6 +12,7 @@ public class Lab3 {
     /**
      *
      * @param args Usage: java Lab3 stops.txt lines.txt HållplatsA HållplatsB
+     *             This is written in the console
      * @throws IOException
      * @throws MalformedData
      */
