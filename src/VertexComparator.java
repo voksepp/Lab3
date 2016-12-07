04b26a84c7cca5293c03e5d4643d2211741a118a
voksepp <fredrik.voksepp@gmail.com>
2016-12-07 22:27:51 +0100

    v1

--- src/VertexComparator.java
+++ src/VertexComparator.java
@@ -4,10 +4,19 @@ import java.util.Map;
 class VertexComparator<E> implements Comparator<Vertex<E>> {
     private Map<Vertex<E>, Integer> d;
 
+    /**
+     * Comparator for comparing the distance to source between two vertices.
+     * @param d Map containing the distances.
+     */
     VertexComparator(Map<Vertex<E>, Integer> d) {
         this.d=d;
     }
 
+    /**
+     * @param o1 The first vertex
+     * @param o2 The second vertex
+     * @return Difference in distance
+     */
     @Override
     public int compare(Vertex<E> o1, Vertex<E> o2) {
         return d.get(o1) - d.get(o2);
